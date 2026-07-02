40d33ee946c8843ce93ec03d7b32d8d68c8338f3
ASCE16Licht <108936567+Licht16ASCE@users.noreply.github.com>
2026-07-02 03:43:05 +0200

    Delete documents directory

--- documents/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Document
-
-
-@admin.register(Document)
-class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('titre', 'type_document', 'sinistre', 'bien', 'created_at')
-    list_filter = ('type_document',)
