a8f43c56457d15c34b5287e79b0f91f90a5838c2
ASCE16Licht <108936567+Licht16ASCE@users.noreply.github.com>
2026-07-02 03:47:36 +0200

    Delete sinistres directory

--- sinistres/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SinistresConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sinistres'
-    verbose_name = 'Sinistres'
